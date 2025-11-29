aa20f95694d3c1eef759972929db36115e2a9aa2
alice <alice@alysia.au>
2025-11-29 18:41:56 +1100

    added critical semicolon

--- flake.nix
+++ flake.nix
@@ -13,7 +13,7 @@
       inputs.nixpkgs.follows = "nixpkgs";
     };
     home-manager = {
-      url = "github:nix-community/home-manager"
+      url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
     };
     dankMaterialShell = {
